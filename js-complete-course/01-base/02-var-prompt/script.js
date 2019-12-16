f0c49b1da3783a6ed164b6234d2e5fec40613b66
Stefan-Nilsson <contact.stefan.nilsson@gmail.com>
2019-12-16 16:29:22 +0100

    change location code 2

--- js-complete-course/01-base/02-var-prompt/script.js
+++ js-complete-course/01-base/02-var-prompt/script.js
@@ -11,6 +11,15 @@
 
 (function() {
 
-    // your code here
+
+    var person = prompt("Please enter your name", "Harry Potter");
+
+    if (person == null || person == "") {
+        txt = "User cancelled the prompt.";
+    } else {
+        txt = "Hello " + person + "! How are you today?";
+    }
+
+
 
 })();
