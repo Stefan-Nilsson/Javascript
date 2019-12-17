c11aae3b8bb4e143a3c6cbdf15c4f0209be08943
Stefan-Nilsson <contact.stefan.nilsson@gmail.com>
2019-12-17 09:57:44 +0100

    add age prompt

--- js-complete-course/01-base/04-asv-confirm/script.js
+++ js-complete-course/01-base/04-asv-confirm/script.js
@@ -9,8 +9,60 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
 
-    // your code here
+
+
+    var age = prompt("What's your age?", "Don't lie!");
+
+    /* if (age) {
+
+        window.confirm ("confragulations")
+
+    } else { confirm("Fine, More cake for me then!")
+
+    }
+
+*/
 
 })();
+
+
+(function () {
+    var age = prompt("What's your age?", "Don't lie!");
+
+})();
+
+
+/*
+
+(function() {
+
+
+    var person = confirm("Do you want some cake?", "No No No");
+
+    if (person) {
+
+        window.confirm ("confragulations")
+
+    } else { confirm("Fine, More cake for me then!")
+
+    }
+
+})();
+
+*/
+
+
+
+
+
+
+
+/*
+
+Ask three questions to the user: his/her Age, Gender, and the Town where he/she lives.
+Display the collected responses in a <em>dialog box</em> and ask to confirm.
+If not confirmed, <strong>re-run the process
+
+ */
