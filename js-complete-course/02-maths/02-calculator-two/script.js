ad0a4c19fe121085fae9bc433999e32b85dfc838
Stefan-Nilsson <contact.stefan.nilsson@gmail.com>
2019-12-17 17:03:56 +0100

    add comments

--- js-complete-course/02-maths/02-calculator-two/script.js
+++ js-complete-course/02-maths/02-calculator-two/script.js
@@ -10,21 +10,42 @@
 // You will have time to focus on it later.
 
 
+// Here I create two variables who contain the value of the two operators (text inputs) in the index.html
+// Because of this I do not have to write the whole code again and again
+// Because of this I can work a lot more efficient and I have a better understanding about what i'm trying to accomplish
+// FIRST declare variable name
+// SECOND =
+// THIRD Number(1239213) Number makes whatever's inside of the brackets a Number class instead of a string class
+// SIDENOTE you can not combine parseInt and Number so if one of both has been used you must continue to use the same type
+// FOURTH document means the whole html page/index.html
+// FIFTH getElementById looks for the id inside the document called whatever's inside the ("tralalala")
+// SIXTH .value looks for the value of the Id for which you looked
 
-var operatorOne =  Number(document.getElementById("op-one").value);
+var operatorOne = Number(document.getElementById("op-one").value);
 var operatorTwo = Number(document.getElementById("op-two").value);
 
-
-
-function calc (calculationTypeIs) {
+// Here I create a function that uses a switch case
+//
+// Function, declares a function, functionname, calc ==> (parameter, this will be case one in case of case one)
+// functionname(caseOne addition) or (caseTwo subtraction) or (caseThree multiplication)
+// functionname(parameterOne, parameterTwo) in this case there's only one parameter
+// do whatever come's inside of the curly brackets
+// { in case of parameter addition do addition, in case of parameter subtraction....}
+//
+function calc(calculationTypeIs) {
     switch (calculationTypeIs) {
-        case 'addition': alert(operatorOne + operatorTwo);
+        case 'addition':
+            alert(operatorOne + operatorTwo);
             break;
-        case 'substraction': alert(operatorOne - operatorTwo);
+        case 'substraction':
+            alert(operatorOne - operatorTwo);
             break;
-        case 'multiplication': alert(operatorOne * operatorTwo);
+        case 'multiplication':
+            alert(operatorOne * operatorTwo);
             break;
-        case 'division': alert(operatorOne / operatorTwo);
-        default: break;
-}
+        case 'division':
+            alert(operatorOne / operatorTwo);
+        default:
+            break;
+    }
 }
