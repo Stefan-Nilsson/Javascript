1b9c28b3fe29f09c93ed0b14e5aec6786483a7d7
Stefan-Nilsson <contact.stefan.nilsson@gmail.com>
2019-12-17 15:49:46 +0100

    add html onclick functionality

--- js-complete-course/02-maths/02-calculator-two/script.js
+++ js-complete-course/02-maths/02-calculator-two/script.js
@@ -9,10 +9,32 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-function test() {
-    return true;
+
+
+switch (stefan) {
+    case 'addition':
+        document.getElementById("addition").addEventListener("click", function() {
+
+            alert(Number(document.getElementById('op-one'.value)) + Number(document.getElementById('op-two'.value)))
+        }
+        break;
+        /*
+    case 'Apples':
+        console.log('Apples are $0.32 a pound.');
+        break;
+    case 'Bananas':
+        console.log('Bananas are $0.48 a pound.');
+        break;
+    case 'Cherries':
+        console.log('Cherries are $3.00 a pound.');
+        break;
+    case 'Mangoes':
+    case 'Papayas':
+        console.log('Mangoes and papayas are $2.79 a pound.');
+        break;
+    default:
+        console.log('Sorry, we are out of ' + expr + '.');
 }
 
-(function() {
-    return true;
-})();
+console.log("Is there anything else you'd like?");*/
+}
