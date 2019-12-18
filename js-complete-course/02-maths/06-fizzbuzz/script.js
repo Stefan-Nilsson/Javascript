841e92de9ff3773c7a2707047f611fceca72927c
Stefan-Nilsson <contact.stefan.nilsson@gmail.com>
2019-12-18 16:02:01 +0100

    finish excercise DONT USE consolelog = ()

--- js-complete-course/02-maths/06-fizzbuzz/script.js
+++ js-complete-course/02-maths/06-fizzbuzz/script.js
@@ -9,8 +9,32 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
 
-    // your code here
+    var i = 100;
+    while (i > 1) {
+        // Will evaluate to true if the variable is divisible by 3
+        if ((i % 3 === 0 && i % 5) === 0) {
+
+            console.log("fizzbuzz")
+        }
+        // Will evaluate to true if the variable is divisible by 5
+        else if (i % 5 === 0) {
+
+            console.log("buzz")
+        }
+
+            // Will evaluate to true if the variable is divisible by 3 & 5
+        // Numbers % 3 === 0 && % 5 === 0;
+
+        else if (i % 3 === 0) {
+
+            console.log("fizz")
+        } else {
+            console.log(i);
+        }
+        i--;
+    }
+
 
 })();
