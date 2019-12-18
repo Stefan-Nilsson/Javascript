3ab35b6b37f8a3f876431907cc94ceaa6009107a
Stefan-Nilsson <contact.stefan.nilsson@gmail.com>
2019-12-18 14:34:36 +0100

    finish factorial, this was difficult for me

--- js-complete-course/02-maths/05-factorial/script.js
+++ js-complete-course/02-maths/05-factorial/script.js
@@ -17,6 +17,18 @@
 
         // your code here
 
-    });
+        var makeNumber = (document.getElementById("number").value);
+        function factorialize(num) {
 
+            if (num === 0 || num === 1)
+                return 1;
+            for (var i = num - 1; i >= 1; i--) {
+                num *= i;
+            }
+            return num;
+        }
+        alert(factorialize(makeNumber));
+
+    });
+;
 })();
