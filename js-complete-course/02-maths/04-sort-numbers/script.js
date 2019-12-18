e7c3f236e1dcf8c16184c90887fcaf6141bd57ae
Stefan-Nilsson <contact.stefan.nilsson@gmail.com>
2019-12-18 12:04:33 +0100

    finished the sorting array method

--- js-complete-course/02-maths/04-sort-numbers/script.js
+++ js-complete-course/02-maths/04-sort-numbers/script.js
@@ -14,9 +14,34 @@
     // to get the value of an input: document.getElementById("element-id").value
 
     document.getElementById("run").addEventListener("click", function() {
+        //
 
-        // your code here
+        var numberList = (document.getElementById("numbers").value);
+        var splitList = numberList.split(", ");
+
+        var sorterList = splitList.sort(function(a, b){return a - b});
+        alert(sorterList);
+
+
+        /* function sortNumbers(a, b) {
+            if (a > b) {
+                return 1;
+            } else if (b > a) {
+                return -1;
+            } else {
+                return 0;
+            }
+        }
+        */
 
     });
 
 })();
+
+
+/*
+
+const numberList = Number(document.getElementById("numbers").value);
+numberList.sort();
+alert(Number(numberList.value));
+*/
