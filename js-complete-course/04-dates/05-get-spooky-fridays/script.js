0b9a914ee3c4c677ff85f1795c7127ee8bc1ec94
Stefan-Nilsson <contact.stefan.nilsson@gmail.com>
2019-12-19 13:53:28 +0100

    in de comments kan je lezen waar ik momenteel mee bezig ben/vastzit, nu neem ik mijn middagpauze

--- js-complete-course/04-dates/05-get-spooky-fridays/script.js
+++ js-complete-course/04-dates/05-get-spooky-fridays/script.js
@@ -11,6 +11,31 @@
 
 (function() {
 
-    // your code here
 
+/* tot nu bezig geweest met logica te onderzoeken van de for loop
+ op:
+ https://medium.com/@omarshishanischool/how-does-a-javascript-for-loop-work-85d2c5d879c8
+ Dit omdat ik momenteel denk om een for loop in een for loop te doen.
+
+ Iets in de aard van:
+(hier jaartal waarin de code moet werken)
+
+ for (i=1;i=12;i++) {
+   if (i=5 && i=13) {
+    alert("ITS FRIDAY BABY")
+
+
+
+ }
+ */
+
+
+/*
+When you click the button,
+display each month of the specified year
+that has a Friday 13th.
+ */
+
+
+  // niet op letten  for (i=1;i<=31;i++) {
 })();
