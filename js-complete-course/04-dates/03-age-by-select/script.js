9bd55194df3e6427b832593724d8a7113903c3af
Stefan-Nilsson <contact.stefan.nilsson@gmail.com>
2019-12-19 12:06:25 +0100

    create var birthYear & currentYear

--- js-complete-course/04-dates/03-age-by-select/script.js
+++ js-complete-course/04-dates/03-age-by-select/script.js
@@ -9,8 +9,16 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
 
     // your code here
+    document.getElementById("dob-year").addEventListener(click), function() {
+      let birthYear = (Number(document.getElementById("dob-year").value));
+      let currentYear = (Number("getFullYear()"));
+
+      
+
+
+    }
 
 })();
