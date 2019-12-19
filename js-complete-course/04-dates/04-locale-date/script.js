bc6409a474378de0c8e4c5202ea91a3ad92ee4b7
Stefan-Nilsson <contact.stefan.nilsson@gmail.com>
2019-12-19 12:48:53 +0100

    add variables

--- js-complete-course/04-dates/04-locale-date/script.js
+++ js-complete-course/04-dates/04-locale-date/script.js
@@ -9,10 +9,23 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
 
-    // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
 
-    // your code here
 
+    const current = new Date();
+    const currentDay = current.getDay();
+    const currentDate = current.getDate();
+    const currentMonth = current.getMonth();
+    const currentYear = current.getFullYear();
+    const currentHour = current.getHours();
+    const currentMinutes = current.getMinutes();
+    const currentHourPlusMinutes = ((currentHour) + ("h") + (currentMinutes))
+    const allDataNicelyPut = ((currentDay) + " " + (currentDate) + " " + (currentMonth) + " " + (currentYear) + " " + (currentHourPlusMinutes));
+    document.getElementById("target").innerHTML = allDataNicelyPut;
+    /* 
+    Change the content of the indicated tag
+    by the date of today, in English
+    (ex : Sunday 28 October 2018, 23h32).
+    */
 })();
