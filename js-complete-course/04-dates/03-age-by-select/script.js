7a3e1a2dcbf8fe83913babddc3bc5b607efbb52c
Stefan-Nilsson <contact.stefan.nilsson@gmail.com>
2019-12-19 12:24:14 +0100

    added some comments to remind me how I did it

--- js-complete-course/04-dates/03-age-by-select/script.js
+++ js-complete-course/04-dates/03-age-by-select/script.js
@@ -11,10 +11,19 @@
 
 (function () {
 
-    // your code here
+    // hier een fout gemaakt bij het maken van de eventlistener
+    // het is .addEventlistener("click"    niet het haakje sluiten, een komma zetten en daarna function()
+    // daarna { hier schrijf je je functie in die bij het klikken moet gebeuren en daarna
+    // afsluiten met } en zeker niet vergeten )
+
     document.getElementById("run").addEventListener("click", function() {
+        // hier maak ik een variabel om het gegeven input jaar op te halen
       const birthYear = (document.getElementById("dob-year").value);
+       // hier maak ik een variabel om een nieuw jaartal of uur of whatever op te halen
       const current= new Date();
+       // dit doe ik door daarna nog een variabel te maken met de vorige + .getFullYear();
+        // Afhankelijk van je gebruik kan je er meerdere maken bvb:
+        // current.getHours(), current.getMinutes()    etc...
       const currentYear = current.getFullYear();
 
       let currentAge = (currentYear - birthYear);
