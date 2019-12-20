37081d4049ece65ae6185ff792fa969c3ff47f03
Stefan-Nilsson <contact.stefan.nilsson@gmail.com>
2019-12-20 11:48:31 +0100

    I FINISHED IT CHECK OUT THIS CODE, please don't

--- js-complete-course/04-dates/05-get-spooky-fridays/script.js
+++ js-complete-course/04-dates/05-get-spooky-fridays/script.js
@@ -14,25 +14,80 @@
 
     document.getElementById("run").addEventListener("click",function () {
 
-        const event = new Date('2019');
+        let event = new Date(document.getElementById("year").value,0,13 );
+        // = new Date('2019');
 
-        event.setFullYear(document.getElementById("year").value);
-        event.setMonth(0);
-        event.setDate(13);
-        let varYear = event.getFullYear();
+       // event.setFullYear(document.getElementById("year").value);
+       // event.setMonth(0);
+       // event.setDate(13);
+       // let varDay = event.getDay();
+      //  let varAlert= event.setMonth();
         let varMonth = event.getMonth();
-        let varDate = event.getDate();
-        let varDay = event.getDay();
-
-        let varMonthChanger = event.setMonth(varMonth++);
-
-        for (varMonth === 1; varMonth ===12; varMonth++) {
-
-            if (varDay === 5) {alert("ITS FRIDAY")}
 
 
+        switch (varMonth) {
 
-
+            case 0:
+                if (event.getDay()== 5) {
+                console.log(varMonth);}
+                varMonth++;
+                event.setMonth(varMonth);
+            case 1:
+                if (event.getDay() == 5) {
+                    console.log(varMonth);}
+                varMonth++;
+                event.setMonth(varMonth);
+            case 2:
+                if (event.getDay() == 5) {
+                    console.log(varMonth);}
+                varMonth++;
+                event.setMonth(varMonth);
+            case 3:
+                if (event.getDay() == 5) {
+                    console.log(varMonth);}
+                varMonth++;
+                event.setMonth(varMonth);
+            case 4:
+                if (event.getDay() == 5) {
+                    console.log(varMonth);}
+                varMonth++;
+                event.setMonth(varMonth);
+            case 5:
+                if (event.getDay() == 5) {
+                    console.log(varMonth);}
+                varMonth++;
+                event.setMonth(varMonth);
+            case 6:
+                if (event.getDay() == 5) {
+                    console.log(varMonth);}
+                varMonth++;
+                event.setMonth(varMonth);
+            case 7:
+                if (event.getDay() == 5) {
+                    console.log(varMonth);}
+                varMonth++;
+                event.setMonth(varMonth);
+            case 8:
+                if (event.getDay() == 5) {
+                    console.log(varMonth);}
+                varMonth++;
+                event.setMonth(varMonth);
+            case 9:
+                if (event.getDay() == 5) {
+                    console.log(varMonth);}
+                varMonth++;
+                event.setMonth(varMonth);
+            case 10:
+                if (event.getDay() == 5) {
+                    console.log(varMonth);}
+                varMonth++;
+                event.setMonth(varMonth);
+            case 11:
+                if (event.getDay() == 5) {
+                    console.log(varMonth);}
+                varMonth++;
+                event.setMonth(varMonth);
+                break;
 
         }
 
@@ -41,13 +96,44 @@
 
 
 
+
+
+        /* let varYear = event.getFullYear();
+        let varAlert = event.getMonth();
+        let varDate = event.getDate();
+        let varDay = event.getDay();
+
+        let varMonthChanger = event.setMonth(varAlert);
+
+
+
+            for(varAlertChanger===1;varAlertChanger <=12;varAlert++) {
+
+                if (varDay ===5) {alert("FRIDAY")}
+                    else {varAlert++}
+
+
+
+            }
+
+*/
+
+
+
+
+
+
+
+
+
+/*
         console.log(event.getFullYear());
         console.log(event.getDay());
         console.log(event.getMonth());
         console.log(event.getDate());
 
 
-
+*/
 
 
 
