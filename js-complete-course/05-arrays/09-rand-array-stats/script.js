75c1f6ff00a7a9e79c690867793742b485f37a73
Stefan-Nilsson <contact.stefan.nilsson@gmail.com>
2019-12-23 11:23:27 +0100

    create vars, rng string

--- js-complete-course/05-arrays/09-rand-array-stats/script.js
+++ js-complete-course/05-arrays/09-rand-array-stats/script.js
@@ -11,6 +11,26 @@
 
 (function() {
 
-    // your code here
+    document.getElementById("run").addEventListener("click",function () {
+
+
+        let varOne = Math.floor(Math.random() * 100);
+        let varTwo = Math.floor(Math.random() * 100);
+        let varThree = Math.floor(Math.random() * 100);
+        let varFour = Math.floor(Math.random() * 100);
+        let varFive = Math.floor(Math.random() * 100);
+        let varSix = Math.floor(Math.random() * 100);
+        let varSeven = Math.floor(Math.random() * 100);
+        let varEight = Math.floor(Math.random() * 100);
+        let varNine = Math.floor(Math.random() * 100);
+        let varTen = Math.floor(Math.random() * 100);
+
+       (document.getElementById('n-1').innerHTML) = varOne;
+
+
+        
+        
+        
+    })
 
 })();
