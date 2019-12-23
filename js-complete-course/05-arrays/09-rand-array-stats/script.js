84a41536837c1056456dd4c65edb1ae99c1f010e
Stefan-Nilsson <contact.stefan.nilsson@gmail.com>
2019-12-23 14:27:21 +0100

    am stuck, trying out different pathways, got the array with 10 rng numbers out and working

--- js-complete-course/05-arrays/09-rand-array-stats/script.js
+++ js-complete-course/05-arrays/09-rand-array-stats/script.js
@@ -9,11 +9,47 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
 
-    document.getElementById("run").addEventListener("click",function () {
+    document.getElementById("run").addEventListener("click", function () {
 
 
+        var rngArray = Array.from({length: 10}, () => Math.floor(Math.random() * 100));
+
+
+        for (i = 1; rngArray.length; i++)
+            (document.getElementById(`n-${i}`).innerHTML) = rngArray[i - 1];
+
+
+
+
+        document.getElementById("min").innerHTML =  rngArray.reduce((bestIndexSoFar, currentlyTestedValue, currentlyTestedIndex, array) => currentlyTestedValue > array[bestIndexSoFar] ? currentlyTestedIndex : bestIndexSoFar, 0);
+    });
+
+
+})();
+
+
+//document.getElementById("min").innerHTML = rngArray.reduce((iMax, x, i, arr) => x > arr[iMax] ? i : iMax, 0);
+
+// document.write("indexOfMaxValue = " + indexOfMaxValue); // prints "indexOfMaxValue = 2"
+
+
+
+/*
+  var peopleResult = people.filter(function(result) {
+      return result.age > 18;})
+
+   */
+/*
+ var minVa
+
+ for (i=1;)
+ (document.getElementById("min").innerHTML) =
+*/
+
+
+/*
         let varOne = Math.floor(Math.random() * 100);
         let varTwo = Math.floor(Math.random() * 100);
         let varThree = Math.floor(Math.random() * 100);
@@ -25,12 +61,4 @@
         let varNine = Math.floor(Math.random() * 100);
         let varTen = Math.floor(Math.random() * 100);
 
-       (document.getElementById('n-1').innerHTML) = varOne;
-
-
-        
-        
-        
-    })
-
-})();
+ */
