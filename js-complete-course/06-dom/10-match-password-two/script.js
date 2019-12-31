6cf8c95a9b6bf6495ee5a267a636b08780424bc4
Stefan-Nilsson <contact.stefan.nilsson@gmail.com>
2019-12-31 12:25:49 +0100

    finished 6.10

--- js-complete-course/06-dom/10-match-password-two/script.js
+++ js-complete-course/06-dom/10-match-password-two/script.js
@@ -10,7 +10,35 @@
 // You will have time to focus on it later.
 
 (function() {
+    document.getElementById("run").addEventListener("click",function () {
+    var password1 = document.getElementById("pass-one").value;
+    var password2 = document.getElementById("pass-two").value;
 
-    // your code here
+
+
+        console.log(password1);
+        console.log(password2);
+
+
+        if (password1 !== password2) {
+            alert ("\nPassword did not match: Please try again...");
+            document.getElementById("pass-one").setAttribute("class", "error");
+            document.getElementById("pass-two").setAttribute("class", "error");
+
+        }
+
+        // If same return True.
+        else{
+            alert("Password Match: Welcome to Stefan's site for Sprite!");
+            return true;
+
+        }
+
+
+
+
+
+
+    })
 
 })();
