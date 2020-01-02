9dd9d0c28b5e66908e7c72e203890b89cb916708
Stefan-Nilsson <contact.stefan.nilsson@gmail.com>
2020-01-02 11:52:16 +0100

    add comments

--- js-complete-course/06-dom/11-change-event-input-one/script.js
+++ js-complete-course/06-dom/11-change-event-input-one/script.js
@@ -10,19 +10,30 @@
 // You will have time to focus on it later.
 
 (function () {
+
+    // alle letters tussen a en Z, g = global, i = case insensitive
     let regExp1 = new RegExp("[a-z]", "gi",);
+    // een variabel voor mijn passwoord inputfield
     let passwordField = document.getElementById("pass-one");
-    let indicatorCount = document.getElementsByClassName("indicator");
+    // een variabel voor de counter
+    let counterCount = document.getElementById("counter");
+    // de maximum lengte van het inputfield
     passwordField.maxLength = 10;
 
-
+    // event listener on Key up (dus wanneer iemand de toets los laat)
     document.getElementById("pass-one").addEventListener("keyup", function () {
+        // hier maak ik een variabel die iedere keer dat je een lettertoets indrukt en los laat
+        // kijkt of het overeenkomt met de regular expression die je eerder definieerde
+        
         let matchChecker = passwordField.value.match(regExp1);
-        document.getElementById("counter").innerHTML = matchChecker.length;
+        counterCount.innerHTML = matchChecker.length;
 
     });
 
 })();
+
+
+
 
 
 /*
