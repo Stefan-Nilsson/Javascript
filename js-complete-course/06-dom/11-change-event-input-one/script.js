6c334ded975dfa08f24c8643fe4adceadbbd8b22
Stefan-Nilsson <contact.stefan.nilsson@gmail.com>
2020-01-02 11:38:47 +0100

    finished excercise 06-11, had some difficulties with the syntax but this was a very learnfull excercise

--- js-complete-course/06-dom/11-change-event-input-one/script.js
+++ js-complete-course/06-dom/11-change-event-input-one/script.js
@@ -9,8 +9,24 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
+    let regExp1 = new RegExp("[a-z]", "gi",);
+    let passwordField = document.getElementById("pass-one");
+    let indicatorCount = document.getElementsByClassName("indicator");
+    passwordField.maxLength = 10;
 
-    // your code here
+
+    document.getElementById("pass-one").addEventListener("keyup", function () {
+        let matchChecker = passwordField.value.match(regExp1);
+        document.getElementById("counter").innerHTML = matchChecker.length;
+
+    });
 
 })();
+
+
+/*
+<label for="pass-one">Password:</label>
+<input type="text" name="pass-one" id="pass-one" />
+<span id="counter" class="indicator">0/10</span>
+ */
