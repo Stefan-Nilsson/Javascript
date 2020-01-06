26d5dea3628583971262a7eae9b977c8f2399553
Stefan-Nilsson <contact.stefan.nilsson@gmail.com>
2020-01-06 12:24:31 +0100

    add randomizer and interval

--- js-complete-course/07-misc/05-worst-ui-three/script.js
+++ js-complete-course/07-misc/05-worst-ui-three/script.js
@@ -11,6 +11,48 @@
 
 (function() {
 
-    // your code here
+    var numberOne = 460;
+    var numberTwo = 0;
+    var numberThree = 0;
+    var numberFour = 0;
+    var number1Text = "460";
+    var number2Text = "00";
+    var number3Text = "00";
+    var number4Text = "00";
 
+    var speed = 100;
+    var speed2 = 80;
+    var setSpeedandRng = setInterval(rngesus, speed);
+    var setSpeedAndRng2 = setInterval(rngesus2, speed2);
+    var setSpeedAndRng3 = setInterval(rngesus3, speed2);
+    var setSpeedAndRng4 = setInterval(rngesus4, speed2);
+
+    function rngesus () {
+        var rngOne = Math.floor(Math.random() * (499 - 460) + 460);
+        document.getElementById('part-one').setAttribute("value",rngOne) ;
+    }
+    function rngesus2 () {
+        var rngTwo = Math.floor(Math.random () * 100);
+        document.getElementById("part-two").setAttribute("value" ,rngTwo);
+    }
+    function rngesus3 () {
+        var rngThree = Math.floor(Math.random () * 100);
+        document.getElementById("part-three").setAttribute("value" ,rngThree);
+    }
+    function rngesus4 () {
+        var rngFour = Math.floor(Math.random () * 100);
+        document.getElementById("part-four").setAttribute("value" ,rngFour);
+    }
+
+    let buttonClassOne = document.querySelectorAll(".button");
+    document.getElementById("target").innerHTML = 460000000;
+    buttonClassOne.forEach(function (button) {
+        buttonClassOne.addEventListener("click", function () {
+
+        });
+        if (button.id === "fix-part-one") {
+            clearInterval(rngesus());
+            console.log("hello baby");
+        }
+    });
 })();
