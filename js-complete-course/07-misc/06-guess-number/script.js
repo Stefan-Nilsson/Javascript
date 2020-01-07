857dcda2e4b02a3d155f632dce53c73bf16f6b76
Stefan-Nilsson <contact.stefan.nilsson@gmail.com>
2020-01-07 10:04:53 +0100

    finished, fun excercise

--- js-complete-course/07-misc/06-guess-number/script.js
+++ js-complete-course/07-misc/06-guess-number/script.js
@@ -11,6 +11,32 @@
 
 (function() {
 
-    // your code here
+let numberToGuess = Math.floor(Math.random () * 100);
+let guessInput = document.getElementById("part-one");
+let tryCounter = 0;
+let guessButton = document.getElementById("fix-part-one");
+let targetThing = document.getElementById("target");
+targetThing.innerHTML = "";
+console.log(numberToGuess);
+
+guessButton.addEventListener("click",function () {
+    if (numberToGuess == guessInput.value) {
+        tryCounter++;
+        targetThing.innerHTML = "That's it, tries: " +  tryCounter;
+    }
+    else if (numberToGuess < guessInput.value) {
+        tryCounter++;
+        targetThing.innerHTML = "Not quite there yet, guess lower, tries: " + tryCounter;
+    }
+    else {
+        tryCounter++;
+        targetThing.innerHTML = "not quite there yet, guess higher, tries: " + tryCounter;
+    }
+
+    console.log(tryCounter);
+    console.log(targetThing.innerHTML);
+    console.log(guessInput.value);
+})
+
 
 })();
