161c1e3a00bc911bfb4b1be59e9f759c648583ee
Stefan-Nilsson <contact.stefan.nilsson@gmail.com>
2020-01-09 17:06:33 +0100

    finished 9.3

--- js-complete-course/09-fetch/03-details/script.js
+++ js-complete-course/09-fetch/03-details/script.js
@@ -9,6 +9,36 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(() => {
-    // your code here
+(function () {
+    document.getElementById("run").addEventListener("click", function () {
+        let heroChosenByPleb = document.querySelector("#hero-id");
+        // heroChosenByPleb.innerHTML = "";
+        let varInput = parseInt(heroChosenByPleb.value);
+        console.log(varInput);
+
+        catchHero().catch(error => {
+            console.log("error");
+            console.error(error);
+        });
+
+        async function catchHero() {
+            const response = await fetch('http://localhost:12345/_shared/api.json');
+            const heroIsChosen = await response.json();
+
+            let varTarget = document.getElementById("target");
+            // console.log(heroIsChosen.heroes[1].name);
+            // console.log(heroChosenByPleb.innerHTML);
+            console.log(heroIsChosen.heroes[0].id);
+            varTarget.innerHTML = heroIsChosen.heroes[varInput].name + "</br>" + heroIsChosen.heroes[varInput].alterEgo + "</br>" + heroIsChosen.heroes[varInput].abilities;
+
+        }
+    });
 })();
+
+
+/*
+        for (i=0; i < heroIsChosen.heroes.length; i++){
+            console.log(heroIsChosen.heroes[i])
+        };
+
+ */
